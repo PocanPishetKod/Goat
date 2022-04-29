17e70c43496ef64c5428577138c5796679c99358
PocanPishetKod <themarcus77@yandex.ru>
2022-04-29 16:25:24 +0300

    Очистка юзингов

--- Goat.Domain/Events/PlayerJoinedEvent.cs
+++ Goat.Domain/Events/PlayerJoinedEvent.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Goat.Domain.Events
+﻿namespace Goat.Domain.Events
 {
     public struct PlayerJoinedEvent
     {
--- Goat.Domain/GameTable.cs
+++ Goat.Domain/GameTable.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Goat.Domain.Events;
 using Goat.EventEngine;
 
